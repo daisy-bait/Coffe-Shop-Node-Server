6d46f59a6c50354a51f45734744d8678ae65eee7
daisy-bait <kadanarpa@gmail.com>
2026-01-18 14:39:16 -0500

    feat: add blog features

--- src/routes/blog.route.js
+++ src/routes/blog.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createBlog, searchBlogByParams } from "../controllers/blog.controller.js";
+import { createBlog, deleteBlogById, searchBlogByParams } from "../controllers/blog.controller.js";
 import { auth } from "../middlewares/auth.middleware.js";
 
 const router = new Router();
@@ -7,5 +7,7 @@ const router = new Router();
 router.post("/create/", auth([]), createBlog);
 
 router.get("/search/", searchBlogByParams);
+
+router.delete("/delete/:id", deleteBlogById);
 
 export default router;
